5244c5a10c54cf38aac8335d0ab5984f5c6dc0b2
Kijin Sung <kijin.sung@gmail.com>
2011-04-08 01:48:54 -0400

    A better way to refresh CSS/JS files when a new version comes out

--- program/views/common_header.php
+++ program/views/common_header.php
@@ -5,13 +5,13 @@
     <title><?php e($title); ?></title>
     <link rel="shortcut icon" type="image/x-icon" href="<?php u('favicon.ico'); ?>" />
     <link rel="stylesheet" type="text/css" media="all" href="<?php u('public/thirdparty/reset-2.0.css'); ?>" />
-    <link rel="stylesheet" type="text/css" media="all" href="<?php u('public/common-' . VERSION . '.css'); ?>" />
-    <!--[if IE 8]><link rel="stylesheet" type="text/css" media="all" href="<?php u('public/ie8sucks-' . VERSION . '.css'); ?>" /><![endif]-->
-    <!--[if IE 9]><link rel="stylesheet" type="text/css" media="all" href="<?php u('public/ie9sucks-' . VERSION . '.css'); ?>" /><![endif]-->
-    <link rel="stylesheet" type="text/css" media="all" href="<?php u('public/user/user.css'); ?>" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php u('public/common.css'); ?>?v=<?php e(VERSION); ?>" />
+    <!--[if IE 8]><link rel="stylesheet" type="text/css" media="all" href="<?php u('public/ie8sucks.css'); ?>?v=<?php e(VERSION); ?>" /><![endif]-->
+    <!--[if IE 9]><link rel="stylesheet" type="text/css" media="all" href="<?php u('public/ie9sucks.css'); ?>?v=<?php e(VERSION); ?>" /><![endif]-->
+    <link rel="stylesheet" type="text/css" media="all" href="<?php u('public/user/user.css'); ?>?v=<?php e(VERSION); ?>" />
     <script type="text/javascript" src="<?php u('public/thirdparty/jquery-1.5.1.min.js'); ?>"></script>
-    <script type="text/javascript" src="<?php u('public/common-' . VERSION . '.js'); ?>"></script>
-    <script type="text/javascript" src="<?php u('public/user/user.js'); ?>"></script>
+    <script type="text/javascript" src="<?php u('public/common.js'); ?>?v=<?php e(VERSION); ?>"></script>
+    <script type="text/javascript" src="<?php u('public/user/user.js'); ?>?v=<?php e(VERSION); ?>"></script>
 </head>
 
 <body class="<?php if (isset($BODY_CLASS)) e($BODY_CLASS); ?>">
